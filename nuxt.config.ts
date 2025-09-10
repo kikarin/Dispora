88a8cc79f0a3499173bed06e6af738043078ca5b
kikarin <akunamazon4811@gmail.com>
2025-09-10 18:49:17 +0700

    rebuild

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -45,7 +45,7 @@ export default defineNuxtConfig({
       wasm: true,
     },
     externals: {
-      inline: [],
+      inline: ['unstorage', 'unstorage/drivers/fs-lite'],
       external: ['detect-libc'],
     },
     alias: {
