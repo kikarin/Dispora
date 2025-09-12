e0687bac5ad3df149c717dad9e7b1b3ceac6ab44
kikarin <akunamazon4811@gmail.com>
2025-09-12 16:31:11 +0700

    Remove ignored files

--- .nuxt/types/middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import type { NavigationGuard } from 'vue-router'
-export type MiddlewareKey = "sanctum:auth" | "sanctum:guest"
-declare module 'nuxt/app' {
-  interface PageMeta {
-    middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
-  }
-}
