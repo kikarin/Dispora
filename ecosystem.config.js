eb5f66c187fd096dba55524ecb439dd153320577
solasoli <azolaakbar@gmail.com>
2025-10-21 12:53:48 +0700

    change node version

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       exec_mode: 'cluster',
       instances: '1', // atau 1 jika server kecil
       script: './.output/server/index.mjs',
-      interpreter: '/root/.nvm/versions/node/v14.7.0/bin/node',
+      interpreter: '/root/.nvm/versions/node/v21.7.3/bin/node',
       env: {
         NODE_ENV: 'production',
         PORT: 3000,
